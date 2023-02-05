09eb3e501193e57879ba4101b1a4cff7b20c9e73
Anton_Kondratev <elkondro@gmail.com>
2023-02-05 22:45:28 +0700

    task_additional solution

--- task_additional/Program.cs
+++ task_additional/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Seminar_3 task_additional\n\n");
+
+Console.Write("input bushes amount: ");
+int bushes = Convert.ToInt32(Console.ReadLine());
+while (bushes < 3 | bushes > 1000) {
+    Console.Write("input correct bushes amount: ");
+    bushes = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] berrys = new int[bushes];
+
+Console.Write("input berrys amount on bushes:\n");
+for (int i = 0; i < berrys.Length; i++) {
+    int berry_amount = Convert.ToInt32(Console.ReadLine());
+    berrys[i] = berry_amount;
+}
+
+int max_harvest = berrys[0] + berrys[1] + berrys[berrys.Length - 1];
+int best_pos = 0;
+
+if (berrys[0] + berrys[berrys.Length - 1] + berrys[berrys.Length - 2] > max_harvest) {
+    max_harvest = berrys[0] + berrys[berrys.Length - 1] + berrys[berrys.Length - 2];
+    best_pos = berrys.Length - 1;
+}
+for (int i = 2; i < berrys.Length; i++) {
+    if (berrys[i-2] + berrys[i-1] + berrys[i] > max_harvest) {
+        max_harvest = berrys[i-2] + berrys[i-1] + berrys[i];
+        best_pos = i - 1;
+    }
+}
+Console.Write($"max_harvest -> {max_harvest}  on {best_pos + 1} bush");
