61bca0ec349cd3938ede96553fa75aa918e1e198
Anton_Kondratev <elkondro@gmail.com>
2023-02-05 21:21:23 +0700

    task_23 solution

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Seminar_3 task_23\n\n");
+
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int n = 1; n <= num; n++) {
+    Console.Write($"{Math.Pow(n, 3)}, ");
+}
+Console.Write("\b\b ");
