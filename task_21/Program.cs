c359e8db456f2425114b8b19c100ab94e1e87a58
Anton_Kondratev <elkondro@gmail.com>
2023-02-05 21:09:43 +0700

    task_21 solution

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Seminar_3 task_21\n\n");
+
+Console.Write("input point Ax: ");
+double ax = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("input point Ay: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("input point Az: ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.Write("\n");
+
+Console.Write("input point Bx: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("input point By: ");
+double by = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("input point Bz: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+double l = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2) + Math.Pow((bz - az), 2));
+Console.Write($"l = {Math.Round(l, 2)}");
