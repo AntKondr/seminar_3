997c74bb6b6f39fbe054e4769b6077d102614b28
Anton_Kondratev <elkondro@gmail.com>
2023-02-05 20:51:46 +0700

    task_19 solution

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Seminar_3 task_19\n\n");
+
+Console.Write("input number: ");
+string num = Console.ReadLine();
+
+int right_index = num.Length - 1;
+bool is_palin = true;
+for (int i = 0; i < (num.Length / 2); i++) {
+    if (num[i] != num[right_index]) {
+        is_palin = false;
+        break;
+    }
+    right_index -= 1;
+}
+if (is_palin) {
+    Console.Write($"{num} -> palindrom");
+}
+else {
+    Console.Write($"{num} -> not palindrom");
+}
